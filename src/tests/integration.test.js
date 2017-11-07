cc485d4fa0761a3a3cd4fd2c3554292f770c6d56
jon.mark.price@gmail.com <Jonathan Price>
2017-11-06 20:37:18 -0600

    Integration tests using Tape.

--- src/tests/integration.test.js
+++ src/tests/integration.test.js
@@ -1,27 +1,28 @@
-import { run } from './helpers';
+const test = require('tape');
+const { run } = require('./helpers');
+
 // This file is for larger tests that do not target a specific function but
 // rather implement somewhat more interesting programs.
-describe('integration tests', () => {
-  it('count the number of even numbers in a list', () => {
-    expect(
-      run([3, 54, 8, 1], 2, '%', 'flip', ':', 'curry', ':', 'map', ':',
-          0, '=', 'curry', ':', 'filter', ':', 'length', ':'))
-      .toEqual(2);
-  });
-
-  it('capitalizes a word', () => {
-    expect(run(['h', 'i'], 'split', ':', [], 'uppercase', [], 'cons', 'curry',
-      ':', 'compose', ':', 'cons', ':', 'id', 'cons', ':', 'zip', ':', 'eval',
-      'map', ':', 'concat', 'apply', ':'
-    )).toEqual(['H', 'i']);
-  });
-
-  it('capitalizes a word using list constructor', () => {
-    expect(run(['h', 'i'], 'split', ':', '[', 'uppercase', [], 'cons',
-      'curry', ':', 'compose', ':', 'id', ']', 'zip', ':', 'eval', 'map', ':',
-      'concat', 'apply', ':'
-    )).toEqual(['H', 'i']);
-  });
+test('count the number of even numbers in a list', (assert) => {
+  assert.equal(
+    run([3, 54, 8, 1], 2, '%', 'flip', ':', 'curry', ':', 'map', ':',
+        0, '=', 'curry', ':', 'filter', ':', 'length', ':'), 2);
+  assert.end();
 });
+
+test('capitalizeis a word', (assert) => {
+  assert.deepEqual(run(['h', 'i'], 'split', ':', [], 'uppercase', [], 'cons',
+    'curry', ':', 'compose', ':', 'cons', ':', 'id', 'cons', ':', 'zip', ':',
+    'eval', 'map', ':', 'concat', 'apply', ':'), ['H', 'i']);
+  assert.end();
+});
+
+test('capitalizes a word using list constructor', (assert) => {
+  assert.deepEqual(run(['h', 'i'], 'split', ':', '[', 'uppercase', [], 'cons',
+    'curry', ':', 'compose', ':', 'id', ']', 'zip', ':', 'eval', 'map', ':',
+    'concat', 'apply', ':'), ['H', 'i']);
+  assert.end();
+});
+
 // TODO:
 // try all examples from src/parser.js
