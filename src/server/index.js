85849e4f883c8c19315b12e9327a57d08cd76350
Jonathan Price <jon.mark.price@gmail.com>
2017-12-06 11:49:28 -0800

    Fixed bug of executing server query only once.

--- src/server/index.js
+++ src/server/index.js
@@ -8,36 +8,21 @@ const saveProgram = require('./save-program');
 const app = express();
 const port = 3000;
 
-const query = User.find({name: 'test'}, (err, docs) =>  
-  new Promise((resolve, reject) => {
-    if (err) {
-      reject(err);
-    } else {
-      resolve(docs);
-    }   
-  })  
-);
-
 app.use('/static', express.static('dist'));
 app.use('/public', express.static('common/public'));
 
 // Don't re-query the database if the data is already in the store.
-let reloads = 0;
 app.get('/', (req, res) => {
-  // if (reloads === 0) {
-  query.then(docs => {
+  User.find({name: 'test'})
+  .exec() // returns a promise
+  .then(docs => {
     if (Array.isArray(docs) && docs.length >= 0) {
-      // XXX: if I run this, it will re-execute all of the functions
-      // in the main scope.
-      // OPTIONS: 
-      // 1. don't separte
-      // 2. move everything in main scope -> 'setup' function
-      //  - PROBLEM: we loose access to store
-      //    - IDEA: pass it as a param?
       res.send(renderPage(docs[0].programs));
     } else {
       res.sendStatus(400);
     }
+  // Pass up data to server
+  // e.g. using res.sendStatus()
   }).catch(err => console.error(err))
   // } else {
     // We've already retrieved the data, so it's already in the store.
