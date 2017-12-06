5bede0787c9582c0e6b9cb9e858500ee1f560333
Jonathan Price <jon.mark.price@gmail.com>
2017-12-06 10:02:02 -0800

    Use mLab sandbox server.

--- src/server/schema.js
+++ src/server/schema.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 mongoose.Promise = Promise;
-mongoose.connect('mongodb://localhost/test', {useMongoClient: true});
+// mongoose.connect('mongodb://localhost/test', {useMongoClient: true});
+mongoose.connect(
+  'mongodb://jon:pw@ds249415.mlab.com:49415/mcilroy',
+  {useMongoClient: true}
+).catch(err => console.err(err));
 
 const userSchema = new Schema({
   name: String,
