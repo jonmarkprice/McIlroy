6884dc2456cf762e0df55ae57895ea9436edf181
Jonathan Price <jon.mark.price@gmail.com>
2017-12-06 15:53:39 -0800

    Use hydrate instead of render in client.

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,8 @@
 const React           = require('react');
-const { render }      = require('react-dom');
+const { hydrate }     = require('react-dom');
 const { Provider }    = require('react-redux');
 const Interpretter    = require('../../shared/components/components');
-const configureStore = require('../common/configureStore');
+const configureStore  = require('../common/configureStore');
 
 console.log('Started successfully...');
 
@@ -13,7 +13,7 @@ delete window.__PRELOADED_STATE__;
 // const store = createStore(reducer, preloadedState);
 const store = configureStore(preloadedState);
 
-render(
+hydrate( // was render
   <Provider store={store}>
     <Interpretter />
   </Provider>,
