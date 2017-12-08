6edf95573a0f6777107628743a9d5cf4b11218ba
Jonathan Price <jon.mark.price@gmail.com>
2017-12-08 15:00:24 -0800

    Error handling based on express-generator.

--- src/server/index.js
+++ src/server/index.js
@@ -1,17 +1,30 @@
-const express     = require('express');
+// TODO: see ./bin/www from express generator
+
 const bodyParser  = require('body-parser');
 const renderPage  = require('./render');
 const { User }    = require('./schema');
 const saveProgram = require('./save-program');
 const deleteProgram = require('./delete-program');
 
+const express     = require('express');
 const jsonParser = bodyParser.json();
+// TODO serve-favicon
+
+// Routes // TODO
+// const index = require('./routes/index');
+// const programs = require('./routes/programs');
+
 const app = express();
 const port = 3000;
 
+// Static routes
 app.use('/static', express.static('dist'));
 app.use('/public', express.static('public'));
 
+// app.use('/', index);
+// app.use('/programs', programs);
+
+// DEPRECATED //
 app.get('/', (req, res, next) => {
   // findOne instead
   User.find({name: 'test'}).exec() // returns a promise
@@ -64,6 +77,24 @@ app.post('/program/rename', jsonParser, (req, res, next) => {
   res.sendStatus(200);
 });
 
+// Catch 404
+app.use(function (req, res, next) {
+  const err = new Error('Not found');
+  err.status = 404;
+  next(err);
+});
+ 
+// Error handler
+app.use(function(err, req, res, next) {
+  // set error iff in dev environment
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+  res.sendStatus(err.status || 500);  
+  // Not using res.render('error'); since we don't have a view engine.
+});
+
+console.log(`Running in: ${app.get('env')}`);
 console.log(`Listening on port ${port}...`);
 app.listen(port);
 
