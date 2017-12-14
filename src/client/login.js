530c0739e4be18c82255ebc3ee1a7234fafcfc52
Jonathan Price <jon.mark.price@gmail.com>
2017-12-13 17:28:14 -0800

    React test sharing Token component.

--- src/client/login.js
+++ src/client/login.js
@@ -1,1 +1,23 @@
+// TODO: use React here
+const React = require('react');
+const ReactDOM = require('react-dom');
+const Token = require('../../build/components/Token');
+
 console.log("Logging on...");
+// const Login = <div>User name: </div>;
+class Login extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>User name: </div>
+        <Token text='Test' />
+      </div>
+    );
+  }
+}
+
+// should be able to use JSX
+ReactDOM.render(
+  <Login />,
+  document.getElementById('app')
+);
