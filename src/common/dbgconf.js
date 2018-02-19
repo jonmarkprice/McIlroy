60cc325efb73ee6849f7eeb6adb5a0fa3c4b318d
Jonathan Price <jon.mark.price@gmail.com>
2018-02-19 15:03:06 +0900

    Turn off debugging; add NODE_ENV=production to serverless environment vars.

--- src/common/dbgconf.js
+++ src/common/dbgconf.js
@@ -1,5 +1,5 @@
 // Debug configuration
 const debug   = require('debug');
-debug.enable('*');
-// debug.disable(); // disable all
+// debug.enable('*');
+debug.disable(); // disable all
 module.exports = debug;
