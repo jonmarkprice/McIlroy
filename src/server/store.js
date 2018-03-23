40aa6f85b96e69eb2e15b2f72f213e8ebd9493df
Jonathan Price <jon.mark.price@gmail.com>
2018-03-23 09:56:45 +0900

    Adding statement to log (unconditionally) on session store set.

--- src/server/store.js
+++ src/server/store.js
@@ -1,4 +1,9 @@
-const dbg = require("debug")("ds:store");
+const debug = require("debug");
+
+debug.enable("always");
+const always = debug("always");
+const dbg = debug("ds:store");
+
 const { Store } = require("express-session");
 const AWS = require("../aws_conf");
 
@@ -47,13 +52,15 @@ class DynamoDBStore extends Store {
         callback(null, null);
       } else {
         dbg("Retrieved from session database: %O", result);
+        dbg("Cookie %O", result.Item.session.cookie);
+        dbg("Passport %O", result.Item.session.passport);
         callback(null, result.Item.session);
       }
     });
   }
 
   set(sid, session, callback) {
-    dbg("Setting session %s, with: %O", sid, session);
+    always("Setting session %s, with: %O", sid, session);
     const params = {
       TableName: this.table,
       Item: {
@@ -72,6 +79,7 @@ class DynamoDBStore extends Store {
     });
   }
 
+  // I wonder if express is re-writing since I have no touch
   // touch(sid, session, callback) // recommended 
 }
 
